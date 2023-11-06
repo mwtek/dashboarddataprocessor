1bcdcf20cd97313ad77f84110bdaeae6cbb58f82
Henner Morten Kruse <hmk@cy01.haemka.lan>
2023-11-06 16:25:56 +0100

    Use submodules and fix script permissions

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 rm -r ~/.m2/repository/de/ukbonn/mwtek
 
 # Build projects
-cd ../utilities && mvn -U clean install &&
-cd ../dashboardlogic && mvn -U clean install &&
-cd ../dashboarddataprocessor && mvn -U clean install
+cd utilities && mvn -U clean install
+cd ../dashboardlogic && mvn -U clean install
+cd .. && mvn -U clean install
